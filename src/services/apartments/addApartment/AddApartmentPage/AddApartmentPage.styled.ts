52b19b139a7a5d86918522a26c128a773996f05c
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2025-07-17 17:37:36 +0300

    [TT-1247]: Add form wrapper

--- src/services/apartments/addApartment/AddApartmentPage/AddApartmentPage.styled.ts
+++ src/services/apartments/addApartment/AddApartmentPage/AddApartmentPage.styled.ts
@@ -4,4 +4,40 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
+  height: calc(100vh - 24px);
+  flex-grow: 1;
+`;
+
+export const Content = styled.div`
+  display: grid;
+  grid-template-columns: 616px 312px;
+  gap: 24px;
+`;
+
+export const FormWrapper = styled.div``;
+
+export const StepsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
+`;
+
+export const StepsTitle = styled.div`
+  font-family: PT Root UI;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 32px;
+  letter-spacing: 0%;
+  vertical-align: middle;
+  color: #272f5a;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end; /* или space-between / center — по желанию */
+  gap: 16px;
+  padding: 24px;
+  border-top: 1px solid #eee;
+  margin-top: auto;
+  width: 616px;
 `;
--- src/services/apartments/addApartment/AddApartmentPage/AddApartmentPage.tsx
+++ src/services/apartments/addApartment/AddApartmentPage/AddApartmentPage.tsx
@@ -1,18 +1,51 @@
 import { FC } from 'react';
-import { Wrapper } from './AddApartmentPage.styled';
+import {
+  ButtonsWrapper,
+  Content,
+  FormWrapper,
+  StepsTitle,
+  StepsWrapper,
+  Wrapper,
+} from './AddApartmentPage.styled';
 import { Props } from './AddApartmentPage.types';
 import { PageHeader } from 'ui-kit/shared/PageHeader';
 import { GoBack } from 'ui-kit/shared/GoBack';
 import { getBuildingAddress } from 'utils/getBuildingAddress';
+import { Steps } from 'antd';
+import { useSteps } from './AddApartmentPage.hooks';
+import { Button } from 'ui-kit/Button';
 
 export const AddApartmentPage: FC<Props> = ({ building }) => {
+  const { step, nextStep } = useSteps(2);
+
   return (
     <Wrapper>
       <GoBack />
       <PageHeader
+        isGhost
         title="Добавление новой квартиры"
         description={getBuildingAddress(building, true)}
       />
+      <Content>
+        <FormWrapper>dasdsa</FormWrapper>
+        <StepsWrapper>
+          <StepsTitle>Этапы создания</StepsTitle>
+          <Steps
+            direction="vertical"
+            current={step}
+            items={[
+              { title: 'Основная информация' },
+              { title: 'Дополнительная информация' },
+            ]}
+          />
+        </StepsWrapper>
+      </Content>
+      <ButtonsWrapper>
+        <Button type='ghost'>Отмена</Button>
+        <Button type="primary" onClick={nextStep}>
+          Далее
+        </Button>
+      </ButtonsWrapper>
     </Wrapper>
   );
 };
