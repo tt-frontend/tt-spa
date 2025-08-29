ccce02e01ebab42729709052b07fed33fb64beeb
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-08-29 15:05:40 +0300

    [TT-1287]

--- src/services/nodes/createNodeService/view/CreateNodePage/ConfigurationConstructor/ConfigurationConstructor.styled.ts
+++ src/services/nodes/createNodeService/view/CreateNodePage/ConfigurationConstructor/ConfigurationConstructor.styled.ts
@@ -1,3 +1,52 @@
 import styled from 'styled-components';
+import dotBg from './Assets/dotBg.svg';
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  background-image: url(${dotBg});
+  width: 112%;
+  height: 100%;
+  margin-left: -100px;
+`;
+
+export const RightBlock = styled.div`
+  display: flex;
+  gap: 16px;
+`;
+
+export const Footer = styled.div`
+  position: fixed;
+  display: flex;
+  bottom: 0px;
+  left: 208px;
+
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 56px;
+  width: calc(100% - 208px);
+  background: #ffffff;
+  box-shadow: 0px -4px 8px 0px rgba(78, 93, 146, 0.16);
+  gap: 16px;
+`;
+
+export const RightPanel = styled.div`
+  position: fixed;
+  display: flex;
+  top: 90px;
+  bottom: 140px;
+  right: 60px;
+
+  padding: 20px 16px;
+  width: 300px;
+  background: #ffffff;
+  box-shadow: 0px -4px 8px 0px rgba(78, 93, 146, 0.16);
+`;
+
+export const TitleText = styled.div`
+  font-weight: 500;
+  font-size: 16px;
+  color: rgba(39, 47, 90, 1);
+`;
+
+export const SchemaWrapper = styled.div`
+  transform: scale(1.3) translateX(260px) translateY(180px);
+`;
--- src/services/nodes/createNodeService/view/CreateNodePage/ConfigurationConstructor/ConfigurationConstructor.tsx
+++ src/services/nodes/createNodeService/view/CreateNodePage/ConfigurationConstructor/ConfigurationConstructor.tsx
@@ -1,7 +1,42 @@
 import { FC } from 'react';
-import { Wrapper } from './ConfigurationConstructor.styled';
+import {
+  Footer,
+  RightBlock,
+  RightPanel,
+  SchemaWrapper,
+  TitleText,
+  Wrapper,
+} from './ConfigurationConstructor.styled';
 import { Props } from './ConfigurationConstructor.types';
+import { Button } from 'ui-kit/Button';
+import { HeatWithRecharge } from './SvgComponents/HeatWithRecharge';
 
-export const ConfigurationConstructor: FC<Props> = () => {
-  return <Wrapper></Wrapper>;
+export const ConfigurationConstructor: FC<Props> = ({
+  //   configurationType,
+  setConfigurationConstructorOpen,
+}) => {
+  return (
+    <Wrapper>
+      <SchemaWrapper>
+        <HeatWithRecharge />
+      </SchemaWrapper>
+
+      <RightPanel>
+        <TitleText>Добавить прибор</TitleText>
+      </RightPanel>
+
+      <Footer>
+        <Button type="ghost">Очистить</Button>
+        <RightBlock>
+          <Button
+            type="ghost"
+            onClick={() => setConfigurationConstructorOpen(false)}
+          >
+            Отмена
+          </Button>
+          <Button>Сохранить</Button>
+        </RightBlock>
+      </Footer>
+    </Wrapper>
+  );
 };
