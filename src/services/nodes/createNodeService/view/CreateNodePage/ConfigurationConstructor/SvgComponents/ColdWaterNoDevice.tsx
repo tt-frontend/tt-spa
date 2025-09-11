95487922d0dc539b47ac649d012edac04f21d23a
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-09-11 18:16:39 +0300

    [TT-1287]

--- src/services/nodes/createNodeService/view/CreateNodePage/ConfigurationConstructor/SvgComponents/ColdWaterNoDevice.tsx
+++ src/services/nodes/createNodeService/view/CreateNodePage/ConfigurationConstructor/SvgComponents/ColdWaterNoDevice.tsx
@@ -1,6 +1,80 @@
 import { FC } from 'react';
+import {
+  AddButton,
+  Block,
+  ModelName,
+  Panel,
+  RightPanel,
+  SchemaWrapper,
+  SerialNumber,
+  TitleText,
+} from '../ConfigurationConstructor.styled';
+import { useUnit } from 'effector-react';
+import { addPipeNodeCommonDeviceService } from 'services/nodes/addPipeNodeCommonDeviceService';
+import { EMagistralType } from 'api/types';
+import { CloseDarkIcon, DeviceIcon } from 'ui-kit/icons';
 import { SvgComponentProps } from '../ConfigurationConstructor.types';
+import { ColdWaterNoDeviceScheme } from './Assets/ColdWaterNoDeviceScheme';
 
-export const ColdWaterNoDevice: FC<SvgComponentProps> = () => {
-  return <></>;
+export const ColdWaterNoDevice: FC<SvgComponentProps> = ({
+  communicationPipes,
+  updateCommonDeviceRequestPayload,
+  handleDeleteDevice,
+}) => {
+  const { openAddCommonDeviceModal } = useUnit({
+    openAddCommonDeviceModal:
+      addPipeNodeCommonDeviceService.inputs.openAddCommonDeviceModal,
+  });
+
+  const feedFlow = communicationPipes.find(
+    (pipe) => pipe.magistral === EMagistralType.FeedFlow,
+  );
+
+  const firstDevice = feedFlow?.devices?.[0];
+
+  return (
+    <>
+      <SchemaWrapper>
+        <ColdWaterNoDeviceScheme
+          updateCommonDeviceRequestPayload={updateCommonDeviceRequestPayload}
+          feedFlow={feedFlow}
+          openAddCommonDeviceModal={openAddCommonDeviceModal}
+        />
+      </SchemaWrapper>
+
+      <RightPanel>
+        <TitleText>Добавить прибор</TitleText>
+        {firstDevice ? (
+          <Panel>
+            <Block>
+              <DeviceIcon />
+              <ModelName>{firstDevice.model}</ModelName>
+              <SerialNumber>({firstDevice.serialNumber})</SerialNumber>
+            </Block>
+
+            <Block>
+              <CloseDarkIcon
+                onClick={() => {
+                  if (feedFlow?.id) {
+                    handleDeleteDevice(feedFlow?.id, 0);
+                  }
+                }}
+              />
+            </Block>
+          </Panel>
+        ) : (
+          <Panel
+            onClick={() => {
+              updateCommonDeviceRequestPayload({
+                pipeId: Number(feedFlow?.id),
+              });
+              openAddCommonDeviceModal();
+            }}
+          >
+            <AddButton> + Добавить прибор</AddButton>
+          </Panel>
+        )}
+      </RightPanel>
+    </>
+  );
 };
