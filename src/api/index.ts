bcc09aa1b9e1dce7450a86fcbfbbeb63d0cbe06b
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-10-23 17:31:47 +0300

    update devUrl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import { createEvent, createStore } from 'effector';
 
-// const devUrl = 'https://prod.k8s.transparent-technology.ru/api/';
-// const devApiURL = localStorage.getItem('dev-api-url');
+const devUrl = 'https://stage.k8s.transparent-technology.ru/api/';
+const devApiURL = localStorage.getItem('dev-api-url');
 
-export const baseURL = 'https://prod.k8s.transparent-technology.ru/api/'; // import.meta.env.VITE_API_URL || devUrl;
+export const baseURL = import.meta.env.VITE_API_URL || devUrl;
 
-const apiURL = 'https://prod.k8s.transparent-technology.ru/api/';
+const apiURL = devApiURL || baseURL;
 
 const setDevUrl = createEvent<string>();
 const $devUrl = createStore(apiURL || '').on(setDevUrl, (_, devUrl) => devUrl);
