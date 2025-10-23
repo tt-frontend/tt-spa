78763b4cbbad313f799c7fa88c6b6768d6d3d946
anotherIlnaz <mmmr.sh.i@gmail.com>
2025-10-23 18:06:16 +0300

    delete devApiURL

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,11 @@
 import { createEvent, createStore } from 'effector';
 
 const devUrl = 'https://stage.k8s.transparent-technology.ru/api/';
-const devApiURL = localStorage.getItem('dev-api-url');
+// const devApiURL = localStorage.getItem('dev-api-url');
 
 export const baseURL = import.meta.env.VITE_API_URL || devUrl;
 
-const apiURL = devApiURL || baseURL;
+const apiURL = baseURL;
 
 const setDevUrl = createEvent<string>();
 const $devUrl = createStore(apiURL || '').on(setDevUrl, (_, devUrl) => devUrl);
