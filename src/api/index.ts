ff3f2f207df13d06f3400678d66745badcf44a72
Iskander Karimov <41307855+pronix575@users.noreply.github.com>
2025-11-11 05:31:58 +0300

    Update

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import { createEvent, createStore } from 'effector';
+import { persist } from 'effector-storage/local';
 
+export const prodUrl = 'https://prod.k8s.transparent-technology.ru/api';
 const devUrl = 'https://stage.k8s.transparent-technology.ru/api/';
-// const devApiURL = localStorage.getItem('dev-api-url');
 
 export const baseURL = import.meta.env.VITE_API_URL || devUrl;
 
@@ -9,6 +10,11 @@ const apiURL = baseURL;
 
 const setDevUrl = createEvent<string>();
 const $devUrl = createStore(apiURL || '').on(setDevUrl, (_, devUrl) => devUrl);
+
+persist({
+  store: $devUrl,
+  key: 'dev-api-url',
+});
 
 export const apiService = {
   outputs: {
